c94c419479cfae7232a5010634f84a707d0533ff
Tarmo Tänav <tarmo@itech.ee>
2010-09-14 19:45:50 +0300

    Don't complain about REXML security patch

--- lib/campaign_monitor.rb
+++ lib/campaign_monitor.rb
@@ -257,7 +257,7 @@ PARSER =
     begin
       require 'rexml-expansion-fix'
     rescue LoadError => e
-      p 'Cannot load rexml security patch'
+      #p 'Cannot load rexml security patch'
     end
     XmlSimple
   end
